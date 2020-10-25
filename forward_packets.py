17a549038d7d8fd7fc0fef3ea8a4a4203d5e3029
byeKhoXx <douha.ismael@gmail.com>
2020-10-25 17:08:31 +0100

    New way to forward the packets

--- forward_packets.py
+++ forward_packets.py
@@ -1,8 +1,8 @@
 import os
 os.sys.path.append('/home/ismael/.local/lib/python3.8/site-packages')# Needed to be executed as root, change the path for the path where scapy is located in your machine 
 
-from scapy.all import send, Raw, sniff, sendp
-from scapy.layers.inet import IP, TCP
+from scapy.all import send, Raw, sniff, sendp, socket
+from scapy.layers.inet import IP, TCP, in4_chksum
 import sys
 #send(IP(dst="10.0.2.5")/TCP(dport=80)/Raw(load="Hola Mundo"), count=5)
 
@@ -16,17 +16,38 @@ SENDER_IP = "10.0.2.6" #IP of the customer of our client
 
 def handle_packet(packet):
     
-
+    #*** NEW APPROACH ***
     #packet[IP].src = MY_IP
     if packet[IP].dst == LOCAL_IP and packet[IP].src == SENDER_IP:
 
+        src = packet[IP].src
+        s_port = packet[TCP].sport
+        dport = packet[TCP].dport
+        print(str(src) + " " + str(s_port) + " " + str(dport))
+
+        send(IP(dst="10.0.2.4")/TCP(dport=8000)/Raw(load=str(src) + " " + str(s_port) + " " + str(dport)))
+        
+
+'''
+        #*** OLD APPROACH *** 
         packet[IP].dst = REMOTE_IP
+        packet[IP].src = LOCAL_IP
+        old_chksum = packet[IP].chksum
         del(packet[IP].chksum)
-        packet.show2()
-        print(packet[IP].src)
-        print(packet[IP].dst)
+
+
+
+        packet_raw = raw(packet)
+        udp_raw = packet_raw[20:]
+        packet[IP].chksum = in4_chksum(socket.IPPROTO_UDP, packet[IP], udp_raw)
+
+
+        packet[IP].show2()
+        new_chksum = packet[IP].chksum
+        print("SRC -> " + packet[IP].src + " /DST -> " + packet[IP].dst)
+        print("OLD -> " + str(old_chksum) + " /NEW -> " + str(new_chksum))
         send(packet)
-        print("Packet redirected!")
+        print("Packet redirected!")'''
 
 
 
