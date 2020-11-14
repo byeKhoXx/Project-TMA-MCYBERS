acf87fcdd1d1d217aaa88727e390f525b1ca37ae
martifercab <martifercab@gmail.com>
2020-11-14 18:02:12 +0100

    Add files via upload

--- receive_packets.py
+++ receive_packets.py
@@ -8,8 +8,8 @@ LOCAL_IP = "10.0.2.4"
 client = input("Client:")
 REMOTE_IP = get_ip_client(client)
 
-# TODO: Define dictionary 1 [ip_src, number of packets]
-# TODO: Define dictionary 2 [ip_src, number of packets]
+global d1 = dict() # Define dictionary 1 [ip_src, number of packets]
+global d2 = dict() # Define dictionary 2 [ip_src, number of packets]
 global packets # Number of packets recived las 15min
 
 # cron ->sched.scheduler  https://docs.python.org/3/library/sched.html
@@ -19,31 +19,50 @@ global packets # Number of packets recived las 15min
 TODO: 
 cron every 1 min:
     r = number of minute % 2
-    if r == 0
-        if dictionary 1 have one entry that have a big number of packets ==> Attack in using the ip ot that entry
-        clean dictionary 1
-    else 
-        if dictionary 2 have one entry that have a big number of packets ==> Attack in using the ip ot that entry
-        clean dictionary 2
 """
+    global d1
+    global d2
+    if r == 0:
+        for key in d1:
+            if d1[key] > 100:
+                # Attack using ip = key
+        d1 = dict()
+    else 
+        for key in d2:
+            if d2[key] > 100:
+                # Attack using ip = key
+        d2 = dict()
 
 # DDoS detection
 """
 TODO:
 cron every 15 min
-    add_packets(client, day, time slot, number of packets)
+    add_packets(client, day, time slot, packets)
     mean10 = mean_last_10(client, time slot)
-    if(packets > mean10 *2) ==> atac
-    packets=0
 """
+    if packets > mean10 *2:
+        # DDoS attack
+    global packets
+    packets=0
+
 
 
 def handle_packet(packet):
     if packet[IP].dst == LOCAL_IP and packet[IP].src == REMOTE_IP:
         packet_raw = raw(packet)
-        # TODO: add to dictionary 1 and 2 the ip recived (if not exist an entry add with 1, and if exist add 1)
+        # Add to dictionary 1 and 2 the ip recived
+        global d1
+        global d2
+        if(packet_raw.ip_src in d1):
+            d1[packet_raw.ip_src] = d1[packet_raw.ip_src] + 1
+        else:
+            d1[packet_raw.ip_src] = 1
+        if(packet_raw.ip_src in d2):
+            d2[packet_raw.ip_src] = d1[packet_raw.ip_src] + 1
+        else:
+            d2[packet_raw.ip_src] = 1
         global packets
-        # TODO: packets++
+        packets = packets + 1
         print("NEW PACKET!")
         print(packet_raw)
 
